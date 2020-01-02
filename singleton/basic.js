75068f129c343d132db49361820cca5547e3e8d8
Marilyn Morales <hi@marilynmorales.com>
2020-01-01 20:32:30 -0600

    singleton

---
+++ singleton/basic.js
@@ -0,0 +1,40 @@
+const mySingleton = (function() {
+	var instance;
+
+	function init() {
+		const privateVar = "I am a private var",
+					privateNumber = Math.random();
+
+		function privateMethod() {
+			return "I am a private method";
+		}
+
+		return {
+			publicMethod: function() {
+				return "The public can see me!"
+			},
+			publicProperty: "I am public",
+
+			getRandomNumber: () => {
+				return privateNumber
+			}
+		}
+	}
+	
+	return {
+		getInstance: () => {
+			if(!instance) {
+				instance = init()
+			}
+
+			return instance;
+		}
+	}
+
+}());
+
+const a = mySingleton.getInstance(),
+			b = mySingleton.getInstance();
+console.log(a === b);
+
+console.log(a.publicMethod(), a.publicProperty, a.getRandomNumber())
